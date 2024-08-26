e7737c7a1d1d41b7fb38ed9ad1a1ebb1733058c0
Kwonyeojiny <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-26 22:49:57 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -8,4 +8,25 @@ function solution(ineq, eq, n, m) {
             return n >= m ? 1:0 ;
         else return n > m ? 1:0 ;
     }
+    
+    
+    // 이런 방법도 있어요
+    
+    // 방법1
+    if (eq === '=' && n === m) return 1
+    if (ineq === '<' && n < m) return 1
+    if (ineq === '>' && n > m) return 1
+    return 0
+    
+    
+    //방법2 객체 생성 
+    const op = operations[ineq + eq];
+    return Number(op(n, m));
 }
+// 방법2 객체
+const operations = {
+  '>=': (n, m) => n >= m,
+  '<=': (n, m) => n <= m,
+  '>!': (n, m) => n > m,
+  '<!': (n, m) => n < m,
+};
