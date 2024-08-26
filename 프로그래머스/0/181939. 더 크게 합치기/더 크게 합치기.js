9c28d940aa791057609eaa106359656048aafc6f
Kwonyeojiny <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-26 21:58:58 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    const ab = String(a) + String(b);
+    const ba = String(b) + String(a);
+    if(Number(ab) < Number(ba))
+        return Number(ba);
+    else return Number(ab);
+
+}
