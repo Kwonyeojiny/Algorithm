76dbc161f61ad3ee1ff4b00b78854e07655129a0
Kwonyeojiny <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-26 22:10:52 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -2,4 +2,9 @@ function solution(a, b) {
     if(Number(String(a)+String(b)) < Number(2*a*b))
         return Number(2*a*b);
     else    return Number(String(a)+String(b));
+    
+    // 이런 방법도 있어요
+    //let num1 = parseInt(a+""+b+"");
+    //let num2 = 2*a*b;
+    //return num1 > num2 ? num1 : num2;
 }
