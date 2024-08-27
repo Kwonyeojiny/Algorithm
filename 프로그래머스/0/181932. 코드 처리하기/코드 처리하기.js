f144bcc5d4fe17d4ebfbba4a24233c48fa299903
Kwonyeojiny <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-27 19:23:23 +0900

    [level 0] Title: 코드 처리하기, Time: 10.28 ms, Memory: 39.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,11 @@
+function solution(code) {
+    let ret ='';
+    let mode = 0;
+    
+    for(i=0;i<code.length;i++){
+        if(mode == 0)
+            code[i] == '1' ? (mode = 1) : (i%2==0 && (ret+=code[i]));
+        else code[i] =='1' ? (mode = 0):(i%2!==0 && (ret+=code[i]));
+    }
+    return (ret.length > 0) ? ret : "EMPTY";
+}
