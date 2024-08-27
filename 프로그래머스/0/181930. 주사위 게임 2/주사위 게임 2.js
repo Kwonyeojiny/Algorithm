27da94fbb0fbbabfe5a6484884aebfa4c8caad71
Kwonyeojiny <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-27 19:50:26 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -2,6 +2,8 @@ function solution(a, b, c) {
     var answer = 0;
 
     //Math.pow(a, 2) -> a*a
+    // a ** 2 -> a*a, a ** 3 -> a*a*a
+    Math.pow([a,b,c],2)
     if(a==b && b==c)
         answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
     else{
