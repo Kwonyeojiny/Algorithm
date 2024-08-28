7b5e013f8306235b3231eb7ce50fe5c9d3f5e6f7
Kwonyeojiny <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-28 22:03:29 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -2,11 +2,14 @@ function solution(n) {
     var answer = [];
     while(n){
         answer.push(n);
-        if(n == 1) break;
-        
+        if(n == 1) return answer;      
         if(n % 2 == 0) n/=2;
         else n = 3*n + 1;
-
     }
-    return answer;
+    
+    // 재귀함수 방법
+    arr.push(n)
+    if (n === 1) return arr
+    if (n % 2 === 0) return solution(n / 2, arr)
+    return solution(3 * n + 1, arr)
 }
