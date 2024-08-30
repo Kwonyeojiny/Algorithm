df0710c4464aeac7d9d0a5068f1730b1bfe464f5
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-08-30 20:59:39 +0900

    [level 0] Title: 배열 만들기 4, Time: 169.22 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -13,5 +13,20 @@ function solution(arr) {
         else if(stk[stk.length -1] >= arr[i])
             stk.pop();       
     }
+    
+    
+    for(let i =0;i<arr.length;){
+        if(stk.length === 0){
+            stk.push(arr[i++]);
+        }
+        else if(stk[stk.length-1] < arr[i]){
+            stk.push(arr[i++]);
+        }
+        else if(stk[stk.length-1] >= arr[i]){
+            stk.pop();
+        }
+    }
+    
+    
      return stk;
 }
