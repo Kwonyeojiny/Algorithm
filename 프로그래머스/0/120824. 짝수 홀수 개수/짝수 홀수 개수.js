bbf1e37d1bba8c0e21d406f5d6e83ea5efdd3b2b
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-07 22:54:26 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,8 +1,9 @@
 function solution(num_list) {
     let answer = [0,0];
     for (i in num_list){
-        if(num_list[i]%2==0) answer[0]++;
-        else answer[1]++;
+        // if(num_list[i]%2==0) answer[0]++;
+        // else answer[1]++;
+        answer[num_list[i]%2]++;
     }
     return answer;
 }
