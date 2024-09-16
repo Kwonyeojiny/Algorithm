dac8a871beff0c69775efdd6f63e31878f38b6ef
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-16 22:49:47 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,8 @@
 function solution(box, n) {
-    return Math.floor(box[0]/n) * Math.floor(box[1]/n) * Math.floor(box[2]/n)
+    //return Math.floor(box[0]/n) * Math.floor(box[1]/n) * Math.floor(box[2]/n)
+    
+    // 구조분해할당
+    const [width, length, height] = box;
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n);
+
 }
