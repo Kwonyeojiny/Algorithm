9c70a7c00c5db22e05866459792cc33a43b27f6b
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-16 22:33:21 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,15 +1,5 @@
 function solution(array) {
-    const arr = array.slice().sort((a, b) => a - b); // 원본 배열을 수정하지 않도록 slice() 사용
-    const max = [];
-    max[0] = arr[arr.length - 1]; // 가장 큰 수 저장
-    
-    // 원본 배열에서 가장 큰 수의 인덱스 찾기
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === max[0]) {
-            max[1] = i; // 인덱스 저장
-            break; // 가장 큰 수의 첫 번째 인덱스만 필요하므로 종료
-        }
-    }
-
-    return max;
+    // Math.max 숫자 중 가장 큰 값 반환 후 큰값의 인덱스
+    let a = Math.max(...array);
+    return [a, array.indexOf(a)]
 }
