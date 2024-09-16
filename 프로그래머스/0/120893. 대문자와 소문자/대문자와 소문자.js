14c34b74ecc5ee77ebfca010cd0485f6d5a2f071
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-16 21:43:32 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.50 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,9 +1,14 @@
 function solution(my_string) {
-    let arr =[]
-    for(let i = 0;i<my_string.length;i++)
-        if(my_string[i] === my_string[i].toUpperCase())
-            arr.push(my_string[i].toLowerCase())
-        else arr.push(my_string[i].toUpperCase())
-    return arr.join('')
+    // let arr =[]
+    // for(let i = 0;i<my_string.length;i++)
+    //     if(my_string[i] === my_string[i].toUpperCase())
+    //         arr.push(my_string[i].toLowerCase())
+    //     else arr.push(my_string[i].toUpperCase())
+    // return arr.join('')
+    
+    let arr ='';
+    for(i in my_string)
+        arr += my_string[i] === my_string[i].toUpperCase() ? my_string[i].toLowerCase() : my_string[i].toUpperCase()
+    return arr
             
 }
