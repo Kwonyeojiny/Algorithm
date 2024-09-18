ce7b25a71ce39e7db07bed061564d2afbea89cf8
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-18 19:25:49 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,10 @@
 function solution(strlist) {
-    let answer = [];
-    for(let i=0;i<strlist.length;i++){
-        answer[i] = strlist[i].length
-    }
-    return answer;
+    // let answer = [];
+    // for(let i=0;i<strlist.length;i++){
+    //     answer[i] = strlist[i].length
+    // }
+    // return answer;
+    
+    // Map 사용
+    return strlist.map((el) => el.length)
 }
