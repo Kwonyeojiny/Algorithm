6f56d7f0dd5da2dd2e168d4b01369b06a37e7a2f
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-18 19:33:40 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,7 @@
+function solution(my_string, n) {
+    let answer = '';
+    for(let i=0;i<my_string.length;i++)
+        for(let j=1;j<=n;j++)
+            answer += my_string[i]
+    return answer
+}
