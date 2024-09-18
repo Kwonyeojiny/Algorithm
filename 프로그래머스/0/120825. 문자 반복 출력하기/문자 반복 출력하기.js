c41b8b2f19834c66f9218869f005f5df0d25a8ba
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-18 19:37:58 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,7 +1,10 @@
 function solution(my_string, n) {
-    let answer = '';
-    for(let i=0;i<my_string.length;i++)
-        for(let j=1;j<=n;j++)
-            answer += my_string[i]
+    // let answer = '';
+    // for(let i=0;i<my_string.length;i++)
+    //     for(let j=1;j<=n;j++)
+    //         answer += my_string[i]
+    
+    // 구조분해할당
+    let answer = [...my_string].map(v => v.repeat(n)).join("");
     return answer
 }
