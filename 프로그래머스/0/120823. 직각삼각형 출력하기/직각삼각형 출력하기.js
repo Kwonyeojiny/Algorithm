0655c100049742f7d81f083a09557eb5db3788f8
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-18 19:23:04 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 68.38 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const num = Number(input[0]); // n값 입력받음
+    let star =''
+    for(let i =1;i<=num;i++){
+        for(let j =1;j<=i;j++)
+            star += '*'
+        star += '\n'
+    }
+    console.log(star)
+});
