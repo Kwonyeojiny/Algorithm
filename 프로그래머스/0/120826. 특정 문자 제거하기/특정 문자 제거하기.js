2fb6102926e9e267e93986edc9e9173a9761dcea
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-20 22:31:41 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,3 +1,5 @@
 function solution(my_string, letter) {
-    return [...my_string].filter((e) => !letter.includes(e)).join('')
+    //return [...my_string].filter((e) => !letter.includes(e)).join('')
+    
+    return my_string.replaceAll(letter, "");
 }
