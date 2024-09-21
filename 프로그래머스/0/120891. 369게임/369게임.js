bf6f6d3d2adf39d6e62933ee9221d4d2aab04f01
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-21 23:17:46 +0900

    Update 369게임.js

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,3 +1,7 @@
 function solution(order) {
     return [...String(order)].filter((e) => ['3','6','9'].includes(e)).length
+
+    return ("" + order).split(/[369]/).length - 1;
+    
+    return [...order.toString().matchAll(/[3|6|9]/g)].length;
 }
