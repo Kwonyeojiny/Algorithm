0d14eae0840f183fbcd1185bcda4a9bebf16ca33
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-23 14:37:16 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,3 +1,11 @@
 function solution(age) {
-    return String(age).split('').map((e)=> String.fromCharCode(Number(e)+97)).join('')
+    // return String(age)
+    //     .split('')
+    //     .map((e)=> String.fromCharCode(Number(e)+97))
+    //     .join('')
+    
+    return String(age)
+    .split('')
+    .map((v) => "abcdefghij"[v])
+    .join("");
 }
