6bb6d8ccd0cd9a83feb77ab2996e85a6c56129c0
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-23 14:08:11 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,9 +1,11 @@
 function solution(my_string) {
-    let arr = ''
-    my_string.split('')
-    for(let i = 0;i<my_string.length;i++){
-        if (arr.indexOf(my_string[i]) === -1)
-            arr += my_string[i]
-    }
-    return arr
+    // let arr = ''
+    // my_string.split('')
+    // for(let i = 0;i<my_string.length;i++){
+    //     if (arr.indexOf(my_string[i]) === -1)
+    //         arr += my_string[i]
+    // }
+    // return arr
+    
+    return [...new Set(my_string)].join('');
 }
