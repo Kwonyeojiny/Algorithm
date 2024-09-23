c8b9b931287969f09b12b2e382071eabdf9d47fb
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-23 16:16:28 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -0,0 +1,6 @@
+function solution(emergency) {
+    // slice로 emergency복사 후 정렬
+    const em = emergency.slice().sort((a,b)=> b-a)
+    // emergency의 요소들이 복사한 배열의 어느 인덱스에 위치하는지
+    return emergency.map((e)=>em.indexOf(e)+1)
+}
