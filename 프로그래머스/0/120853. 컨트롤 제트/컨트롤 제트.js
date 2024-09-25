038d15b2161aeec75886f31523d47cefcad7e95d
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-25 11:32:14 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,10 +1,16 @@
 function solution(s) {
-    const s_arr = s.split(' ')
+    // const s_arr = s.split(' ')
     let arr = []
-    for(let i=0;i<s_arr.length;i++){
-        s_arr[i] === 'Z' ? arr.pop(s_arr[i-1]) : arr.push(s_arr[i])
-    }
-    const sum = arr.reduce((acc,num) => +acc + +num,0)
-    return sum
+    // for(let i=0;i<s_arr.length;i++){
+    //     s_arr[i] === 'Z' ? arr.pop(s_arr[i-1]) : arr.push(s_arr[i])
+    // }
+    // const sum = arr.reduce((acc,num) => +acc + +num,0)
+    // return sum
+    
+    s.split(' ').forEach((target) => {
+        (target === 'Z') ? arr.pop() : arr.push(+target)
+    })
+
+    return arr.reduce((pre, cur) => +pre + +cur,0) 
 
 }
