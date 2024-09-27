03fd7d61c12d8f99bd03ffd4e6f1e3c07ebe6150
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-09-28 01:23:08 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,15 +1,20 @@
 function solution(dots) {
     // 1차원 배열의 인덱스가 0인 것끼리 비교 x축 값이 작은 것 앞으로 옴
-    dots.sort((a,b) => a[0] - b[0])
-    // [ [x 작은 놈, y],[x 작은 놈, y],[x 큰 놈, y],[x 큰 놈, y] ]
-    // [[1, 1], [1, 2], [2, 1], [2, 2] ]
+//     dots.sort((a,b) => a[0] - b[0])
+//     // [ [x 작은 놈, y],[x 작은 놈, y],[x 큰 놈, y],[x 큰 놈, y] ]
+//     // [[1, 1], [1, 2], [2, 1], [2, 2] ]
     
-    const row = Math.abs(dots[0][1] - dots[1][1])
-    const col = Math.abs(dots[0][0] - dots[2][0])
+//     const row = Math.abs(dots[0][1] - dots[1][1])
+//     const col = Math.abs(dots[0][0] - dots[2][0])
     
     // const [[x1,y1],[x2,y2],[x3,y3],[x4,y4]] = dots
     // const row = Math.max(x1,x2,x3,x4) - Math.min(x1,x2,x3,x4)
     // const col = Math.max(y1,y2,y3,y4) - Math.min(y1,y2,y3,y4)
     
-    return row * col
+    // return row * col
+    
+    const x = [...new Set(dots.map(p => p[0]))]
+    const y = [...new Set(dots.map(p => p[1]))]
+    
+    return Math.abs((x[0] - x[1]) * (y[0] - y[1]))
 }
