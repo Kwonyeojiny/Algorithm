5dd9c1d196097bb9c30c85e8af37715364eebd34
권여진이 <78148876+Kwonyeojiny@users.noreply.github.com>
2024-10-01 22:28:36 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,7 +1,11 @@
 function solution(my_str, n) {
     const arr = []
     for(let i= 0 ;i<my_str.length;i+=n){
-        arr.push(my_str.slice(i,i+n))
+        arr.push(my_str.substr(i, n))
     }
     return arr
 }
+
+
+// slice(i,i+n)
+// substr(i, n)
