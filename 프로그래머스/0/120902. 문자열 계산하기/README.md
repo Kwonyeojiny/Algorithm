7ee4fbeb56464f3d4de41ff13f04c129b02a6c06
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2024-10-07 15:40:10 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.09 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/README.md
+++ 프로그래머스/0/120902. 문자열 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 계산하기 - 120902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.10 ms
+메모리: 32.9 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 07일 15:32:31
+2024년 10월 07일 15:40:08
 
 ### 문제 설명
 
--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,9 +1,12 @@
 function solution(my_string) {
-    const arr = my_string.split(' ')
-    let first = Number(arr[0])
-    arr.forEach((item,index)=>{
-        if(item === '+') first += Number(arr[index+1])
-        if(item === '-')  first -= Number(arr[index+1])
-    })
-    return first
+    // const arr = my_string.split(' ')
+    // let first = Number(arr[0])
+    // arr.forEach((item,index)=>{
+    //     if(item === '+') first += Number(arr[index+1])
+    //     if(item === '-')  first -= Number(arr[index+1])
+    // })
+    // return first
+    
+    // 문제는 풀리지만 쓰지말라는 코드 eval
+    return eval(my_string)
 }
