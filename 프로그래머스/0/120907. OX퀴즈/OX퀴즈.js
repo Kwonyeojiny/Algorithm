bb7c1b00aadc36fb3a01cdf1770fae656adbfcd2
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2024-10-13 22:44:52 +0900

    [level 0] Title: OX퀴즈, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,16 +1,26 @@
 function solution(quiz) {
-    const answer = [];
-    quiz.forEach((item)=>{
-        const q =  item.split(" ")
+//     const answer = [];
+//     quiz.forEach((item)=>{
+//         const q =  item.split(" ")
         
-        const a = +q[0];
-        const b = +q[2];
+//         const a = +q[0];
+//         const b = +q[2];
         
-        const result = (q[1] === '+') ? a+b : a-b
+//         const result = (q[1] === '+') ? a+b : a-b
         
-        const r = (q[4] == result) ? "O" : "X"
-        answer.push(r)
-    })
+//         const r = (q[4] == result) ? "O" : "X"
+//         answer.push(r)
+//     })
     
-    return answer
+//     return answer
+    
+
+    var answer = [];
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    });
 }
