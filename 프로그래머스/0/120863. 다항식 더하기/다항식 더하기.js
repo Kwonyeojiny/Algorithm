6f38d5c6a6c5565d7e924d01138d83ea807e6cb8
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2024-10-21 00:05:39 +0900

    [level 0] Title: 다항식 더하기, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,24 +1,19 @@
 function solution(polynomial) {
-    const po = polynomial.split(' + ');
-    let xNum = 0;
-    let Num = 0;
-
-    po.forEach(n => {
-        if (n.includes('x')) {
-            const parts = n.split('x');
-            if (parts[0] === "" || parts[0] === "1") {
-                xNum += 1; // 계수가 1인 경우 또는 "x"인 경우
-            } else {
-                xNum += Number(parts[0]); // 다른 계수인 경우
-            }
-        } else {
-            Num += Number(n); // 상수항인 경우
+    const po = polynomial.split(' + ')
+    let xNum = 0
+    let Num = 0
+    po.map(n => {
+        if(n.includes("x")){
+            const spl = n.split('x')
+            if(spl[0] === "") xNum += 1
+            else xNum += Number(spl[0])
         }
-    });
+        else Num += Number(n)
+    })
 
-    const arr = [];
-    if (xNum !== 0) arr.push(`${xNum === 1 ? "" : xNum}x`);
-    if (Num !== 0) arr.push(Num);
-
-    return arr.length > 0 ? arr.join(' + ') : "0";
+    const arr = []
+    if (xNum !== 0 ) arr.push(`${xNum === 1 ? "" : xNum }x`)
+    if (Num !== 0) arr.push (Num)
+    
+    return arr.join(' + ')
 }
