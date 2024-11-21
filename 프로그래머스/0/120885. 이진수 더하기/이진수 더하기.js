9d4366da1fa7fd5afce5c395eef07838c82f6197
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2024-11-21 23:03:54 +0900

    [level 0] Title: 이진수 더하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,4 +1,21 @@
 function solution(bin1, bin2) {
-    const answer = (parseInt(bin1,2) + parseInt(bin2,2)).toString(2)
-    return answer;
+
+    // 이진수 의도의 코드?
+    let temp = Number(bin1) + Number(bin2);
+    temp = [...temp.toString()].reverse().map((v) => +v);
+
+    for (let i = temp.length; i < 11; i++) {
+        temp.push(0);
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] === 2) {
+            temp[i] = 0;
+            temp[i + 1]++;
+    } else if (temp[i] === 3) {
+        temp[i] = 1;
+        temp[i + 1]++;
+    }
+  }
+  return Number(temp.reverse().join("")).toString();
 }
