29cf989ea253866d66efab601bc9972c48449066
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2025-03-11 17:28:40 +0900

    [level 2] Title: H-Index, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,11 +1,3 @@
-function solution(citations) {
-    const arr = citations.sort((a,b)=>b-a);
-    let answer = 0;
-    
-    for(let i = 0; i<arr.length;i++){
-        if(arr[i]>i) answer++;
-        else break;
-    }
-    return answer;
-    
+function solution(citations) {    
+    return citations.sort((a,b)=>b-a).filter((el,idx)=> el>idx).length;;
 }
