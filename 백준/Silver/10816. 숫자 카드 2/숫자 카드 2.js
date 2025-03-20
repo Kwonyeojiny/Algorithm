5dd94b14ff494776a8cf829efea91b7824ba1a0f
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2025-03-20 14:15:30 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 688 ms, Memory: 172056 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const arrN = input[1]
-  .split(" ")
-  .sort((a, b) => b - a)
-  .map(Number);
+const arrN = input[1].split(" ").map(Number);
 const arrM = input[3].split(" ").map(Number);
 
-let answer = [];
-let Card = new Map();
+let cardCount = new Map();
 
-for (num of arrN) {
-  if (Card.has(num)) Card.set(num, Card.get(num) + 1);
-  else Card.set(num, 1);
+for (let num of arrN) {
+  if (cardCount.has(num)) {
+    cardCount.set(num, cardCount.get(num) + 1);
+  } else {
+    cardCount.set(num, 1);
+  }
 }
 
-for (num of arrM) {
-  if (Card.has(num)) answer.push(Card.get(num));
-  else answer.push(0);
+let result = [];
+
+for (let num of arrM) {
+  result.push(cardCount.get(num) || 0);
 }
 
-console.log(answer.join(" "));
+console.log(result.join(" "));
