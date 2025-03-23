0c1a0dbda751cc08f74694e7b674dc69ba90e09e
Yeojin Kwon <78148876+Kwonyeojiny@users.noreply.github.com>
2025-03-23 15:50:25 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 316 ms, Memory: 54680 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -2,13 +2,27 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [N, M] = input[0].split(" ").map(Number);
-const poketmons = input.slice(1, N + 1);
-const questions = input.slice(N + 1);
 
-const map = new Map(
-  poketmons.map((poketmons, index) => [poketmons, index + 1])
-);
-questions.forEach((question) => {
-  if (Number.isNaN(+question)) console.log(map.get(question));
-  else console.log(poketmons[+question - 1]);
-});
+const nameList = [];
+const nameMap = new Map();
+
+for (let i = 1; i <= N; i++) {
+  const name = input[i];
+  nameList[i] = name;
+  nameMap.set(name, i);
+}
+
+const result = [];
+
+for (let i = N + 1; i <= N + M; i++) {
+  const query = input[i];
+
+  if (!isNaN(query)) {
+    const idx = Number(query);
+    result.push(nameList[idx]);
+  } else {
+    result.push(nameMap.get(query));
+  }
+}
+
+console.log(result.join("\n"));
