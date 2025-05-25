e844396771eca268e40276bf1f7565a9da6a9721
Yeojin Kwon <yeojinious@gmail.com>
2025-05-25 19:43:51 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.58 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+function solution(s)
+{
+    let stack = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] !== stack[stack.length-1]) stack.push(s[i]);
+        else stack.pop();
+    }
+    
+    return stack.length ? 0 : 1;
+}
